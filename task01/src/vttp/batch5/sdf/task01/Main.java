19d0a236847a389190f3c473246463ddcf3127f9
Jo Yie <joyieleong@gmail.com>
2024-10-30 10:37:48 +0800

    task01 complete

--- task01/src/vttp/batch5/sdf/task01/Main.java
+++ task01/src/vttp/batch5/sdf/task01/Main.java
@@ -1,12 +1,98 @@
 package vttp.batch5.sdf.task01;
 
+import java.util.*;
+import java.io.*;
+
+import vttp.batch5.sdf.task01.models.Bike;
+
 // Use this class as the entry point of your program
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-		System.out.printf("hello, world\n");
+		// instantiate BikeEntry class
+		Bike be = new Bike();
+
+		// store BikeEntry(s) in List<BikeEntry> bikeList
+		List<Bike> bikeList = new ArrayList<>();
+
+		// create new File
+		File file = new File("day.csv"); 
+
+		// decorator pattern
+		FileReader fr = new FileReader(file);
+		BufferedReader br = new BufferedReader(fr);
+
+		// ignore first line of csv file
+        String headerLine = br.readLine(); 
+
+		String temp = ""; 
+		while ((temp = br.readLine()) != null) {
+
+			// split each line in csv file by comma 
+			String[] line = temp.split(",");
+			
+			// for each line --> new BikeEntry() 
+			Bike tempBe = be.toBikeEntry(line);
+
+			// put entry into bikeList
+			bikeList.add(tempBe);
+
+		}
+
+		// sort bikeList by num casual + registered users (totalCyclists)
+		Comparator<Bike> compare = Comparator.comparing(Bike::getTotalCyclists, Comparator.reverseOrder());
+		bikeList.sort(compare);
+
+		// select top 5 days and print 
+		for (int i = 0; i < 5; i++) {
+			Bike b = bikeList.get(i);
+			printDetails(b, i);
+
+			System.out.println();
+
+		}
 
 	}
+
+	// print details 
+	public static void printDetails(Bike b, int pos) {
+
+		// instantiate new Utilities object 
+		Utils ut = new Utils();
+
+		String position = "";
+
+		if (pos == 0) {
+			position = "highest"; 
+		} else if (pos == 1) {
+			position = "second highest";
+		} else if (pos == 2) {
+			position = "third highest";
+		} else if (pos == 3) {
+			position = "fourth highest";
+		} else if (pos == 4) {
+			position = "fifth highest";
+		}
+
+		String season = ut.toSeason(b.getSeason()); 
+		String day = ut.toWeekday(b.getWeekday()); 
+		String month = ut.toMonth(b.getMonth()); 
+		int total = b.getTotalCyclists(); 
+		String weather = ut.toWeather(b.getWeather());
+		boolean holiday = b.isHoliday();
+
+		String holidayStr; 
+
+		if (holiday) {
+			holidayStr = "a holiday";
+		} else {
+			holidayStr = "not a holiday";
+		}
+
+		System.out.printf("The %s recorded number of cyclists was in %s, on a %s in the month of %s. There were a total of %d cyclists. The weather was %s.\n%s was %s.\n", position, season, day, month, total, weather, day, holidayStr);
+
+	}
+
 }
---
+++ task01/src/vttp/batch5/sdf/task01/Utils.java
@@ -0,0 +1,79 @@
+package vttp.batch5.sdf.task01;
+
+// MODIFIED UTILITIES CLASS
+
+public class Utils {
+
+    public static final String[] SEASON = { "Spring", "Summer", "Fall", "Winter" };
+	public static final String[] DAY = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+	public static final String[] MONTH = { "January", "Febuary", "March", "April", "May",
+		"June", "July", "August", "September", "October", "November", "December"
+	};
+    public static final String[] WEATHER = {"Clear, Few clouds, Partly cloudy, Partly cloudy",
+                                            "Mist + Cloudy, Mist + Broken clouds, Mist + Few clouds, Mist",
+                                            "Light Snow, Light Rain + Thunderstorm + Scattered clouds, Light Rain + Scattered clouds",
+                                            "Heavy Rain + Ice Pallets + Thunderstorm + Mist, Snow + Fog"};
+
+	public static String toSeason(int season) {
+		//season (1:spring, 2:summer, 3:fall, 4:winter)
+		switch (season) {
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+				return SEASON[season - 1];
+			default:
+				return "funny season";
+		}
+	}
+
+	public static String toWeekday(int weekday) {
+		switch (weekday) {
+			case 0:
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+			case 5:
+			case 6:
+				return DAY[weekday - 1];
+			default:
+				return "incorrect day";
+		}
+	}
+
+	public static String toMonth(int month) {
+		switch (month) {
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+			case 5:
+			case 6:
+			case 7:
+			case 8:
+			case 9:
+			case 10:
+			case 11:
+			case 12:
+				return MONTH[month - 1];
+			default:
+				return "unknown month";
+		}
+	}
+
+    public static String toWeather(int weather) {
+        // 1, 2, 3, 4
+        switch (weather) {
+            case 1:
+            case 2:
+            case 3: 
+            case 4: 
+                return WEATHER[weather - 1];
+            default: 
+                return "incorrect weather";
+        }
+
+    }
+
+}
